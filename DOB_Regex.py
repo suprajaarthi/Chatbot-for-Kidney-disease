1d2d02191986ccdd4cf2f11d51746fb469c4cac7
Supraja Arthi S <51900501+suprajaarthi@users.noreply.github.com>
2021-03-20 16:27:44 +0530

    Update DOB_Regex.py

--- DOB_Regex.py
+++ DOB_Regex.py
@@ -3,3 +3,4 @@ string = "John 34-3456 12-05-2007 , XYZ 56-4532 11-11-2011 , ABC 67-945 12-01-20
 pattern =r'\d{2}-\d{2}-\d{4}'
 res=re.findall(pattern,string);
 print(res)
+# ['12-05-2007', '11-11-2011', '12-01-2009']
